ffe3d5a519bda9b6dfa7ec5d552eb55934065307
Ari Mayer <ari111097@gmail.com>
2019-11-04 21:35:49 -0500

    I keep making changes, I don't know why

--- visualization.js
+++ visualization.js
@@ -57,6 +57,8 @@ d3.csv("data/MBTA_GTFS_csv/RouteShapes.csv").then(function(data) {
 
 var mymap = L.map('mapid').setView([42.3530, -71.0770], 13);
 
+//We inspected and pull access token for the rights to a map straight from leaflet
+
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.' +
     'png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
